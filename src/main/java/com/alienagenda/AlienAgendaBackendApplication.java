8074884186b95df1bf39ecb3ba70c50051a78f51
Lucas J. Dutra <lucasjdc90@gmail.com>
2024-12-10 00:28:24 -0300

    initial commit

---
+++ src/main/java/com/alienagenda/AlienAgendaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alienagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienAgendaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienAgendaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alienagenda/AlienAgendaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alienagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlienAgendaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
