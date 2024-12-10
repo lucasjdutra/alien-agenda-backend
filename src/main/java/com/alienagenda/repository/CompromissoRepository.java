9ee60760409c5785b6eb27c55057210744f0d38e
Lucas J. Dutra <lucasjdc90@gmail.com>
2024-12-10 00:35:47 -0300

    added repository

---
+++ src/main/java/com/alienagenda/repository/CompromissoRepository.java
@@ -0,0 +1,7 @@
+package com.alienagenda.repository;
+
+import com.alienagenda.model.Compromisso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompromissoRepository extends JpaRepository<Compromisso, Integer> {
+}
