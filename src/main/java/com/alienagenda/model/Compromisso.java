18736b4cab67d4a1f66972c21a3853910230d742
Lucas J. Dutra <lucasjdc90@gmail.com>
2024-12-10 00:31:31 -0300

    added entity Compromisso

---
+++ src/main/java/com/alienagenda/model/Compromisso.java
@@ -0,0 +1,24 @@
+package com.alienagenda.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Compromisso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String data; 
+    private String horario; 
+    private String descricao;
+}
